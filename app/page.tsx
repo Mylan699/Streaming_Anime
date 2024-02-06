0400d55d669d29004fc2a04ca10b371abbe67d16
Mylan699 <mylan.mahieddine@ecole-isitech.fr>
2024-02-06 13:53:11 +0100

    06/02/2024 13:53

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 import LoadMore from "@/components/LoadMore";
-//import { data } from "./_data"; Enlever le commentaire et supprimer la ligne 7 pour avoir uniquement les image prédefinis
 import { fetchAnime } from "@/app/action";
 
 async function Home() {
@@ -11,9 +10,7 @@ async function Home() {
       <h2 className="text-3xl text-white font-bold">Découvrir</h2>
 
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <LoadMore />
     </main>
